28296deaf42a302b5a5056f6109bfd151b037c0c
OlegTemek <olegtemek@gmail.com>
2023-06-02 16:11:17 +0600

    add link to telegram bot on the client

--- client/components/TheHeader.vue
+++ client/components/TheHeader.vue
@@ -4,6 +4,9 @@
       <h1><img src="@/assets/source/main-logo.svg" alt="OLEGTEMEK portfolio minecraft logo" /></h1>
       <span>Hello world!</span>
     </div>
+    <div class="header__info">
+      <h2>You can use telegram bot: <a href="https://t.me/OlegTemekPortfolioBot">@OlegTemekPortfolioBot</a></h2>
+    </div>
   </header>
 </template>
 
